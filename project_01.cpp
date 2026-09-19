926c1c23c4b5c8620150c09d48bd9da6c0411771
Rambabu kumar <rambabukumar748858@gmail.com>
2026-09-19 10:53:46 +0530

    Add project_02

---
+++ project_01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the n number : ";
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "Even number";
+    }
+    else
+    {
+        cout << "Odd number";
+    }
+    return 0;
+}
---
+++ project_02.py
@@ -0,0 +1,9 @@
+# Leap Year Checker
+
+year = int(input("Enter the year : "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year," is leap year")
+else:
+    print(year, " is not leap year")
+
+
